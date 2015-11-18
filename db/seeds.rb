9122cb5cc3c6c9c860738143d28c406fa1ef4142
Jesse Caldwell <CollectionOfAtoms@gmail.com>
2015-11-18 12:35:53 -0800

    Add account activations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,17 @@ User.create!(name: "Example User",
 						 email: "example@railstutorial.org",
 						 password: 							"foobar",
 						 password_confirmation: "foobar",
-						 admin: true)
+						 admin: true,
+						 activated: true,
+						 activated_at: Time.zone.now)
 
 User.create!(name: "Jesse Caldwell",
 						 email: "CollectionOfAtoms@gmail.com",
 						 password: 							"suckmydick",
 						 password_confirmation: "suckmydick",
-						 admin: true)
+						 admin: true,
+						 activated: true,
+						 activated_at: Time.zone.now)
 
 99.times do |n|
 	name = Faker::Name.name
@@ -20,6 +24,8 @@ User.create!(name: "Jesse Caldwell",
 	User.create!(name: name, 
 							 email: email,
 							 password:              password,
-							 password_confirmation: password)
+							 password_confirmation: password,
+							 activated: true,
+							 activated_at: Time.zone.now)
 end
 
