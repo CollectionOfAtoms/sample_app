feb1d325ebe3d04e5bf89c137af54e9d48f0ee7f
Jesse Caldwell <CollectionOfAtoms@gmail.com>
2015-12-03 12:15:34 -0800

    Fix heroku deploy problem

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,11 +1,15 @@
 if Rails.env.production?
+	config.root = Rails.root.join('tmp') # adding these...
+  config.cache_dir = 'carrierwave' # ...two lines
+
   CarrierWave.configure do |config|
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AKIAIMXE7ZQ6UUEBGL4Q'],
-      :aws_secret_access_key => ENV['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']
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY'],
+      :region                => ENV['S3_REGION']
     }
-    config.fog_directory     =  ENV['jesse-sample-app-photos']
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
